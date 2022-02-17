52656ab380ad36c2b12858d7ddebdedb018d8b41
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-02-17 12:44:18 +0900

    practice 220217

---
+++ section04-1.py
@@ -0,0 +1,75 @@
+# 데이터 타입
+
+from unittest import result
+
+
+v_str1 = "Niceman"
+v_bool = True
+v_str2 = "Goodboy"
+v_float = 10.3
+v_int = 7
+v_sict = {
+  "name" : "Kim",
+  "age" : 25
+}
+
+v_list = [3, 5, 7]
+v_tuple = 3, 5, 7
+v_set = {7, 8, 9}
+
+print(type(v_tuple))
+print(type(v_set))
+print(type(v_float))
+
+i1 = 39
+i2 = 939
+big_int1 = 99999999999999999999999999999999
+big_int2 = 77777777777777777777777777777777
+f1 = 1.234
+f2 = 3.784
+f3 = .5
+f4 = 10.
+
+print(i1 * i2)
+print(big_int1 * big_int2)
+print(f1 ** f2)
+print(f3 + i2)
+
+result = f3 + i2
+print(result, type(result))
+
+a = 5.
+b = 4
+c = 10
+d = 3.15
+
+print(type(a), type(b))
+result2 = a + b
+print(result2)
+
+# 형변환
+# int, float, complex(복소수)
+
+print(int(result2))
+print(float(c))
+print(complex(3))
+print(complex(d))
+print(int(True))
+print(int(False))
+print(int('3'))
+print(complex(False))
+
+y = 100
+y += 100
+print(y)
+
+# 수치 연산 함수
+# https://docs.python.org/3/library/math.html
+
+print(abs(-7))
+n, m = divmod(100, 8) # 100을 8로 나눠서 몫과 나머지를 배분
+print(n, m)
+
+import math 
+print(math.ceil(5.1)) # 해당 값보다 크면서 가장 작은 정수
+print(math.floor(3.874)) # 해당 값보다 작으면서 가장 큰 정수
